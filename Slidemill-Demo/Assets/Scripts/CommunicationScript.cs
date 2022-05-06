b75953ace7770ecd014f9041360e3c9c1cddf018
NemeanLion42 <59586599+NemeanLion42@users.noreply.github.com>
2022-05-06 01:31:06 -0400

    Switch to better IMU and get good enough performance for demo

--- Slidemill-Demo/Assets/Scripts/CommunicationScript.cs
+++ Slidemill-Demo/Assets/Scripts/CommunicationScript.cs
@@ -12,6 +12,9 @@ public class CommunicationScript : MonoBehaviour
     private float acc_threshold = 0.25f;
     private float timeSinceLastMessage;
     public TMPro.TextMeshPro timeDisplay;
+    public GameObject foot;
+    public Vector3 startPosition;
+    public bool move;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +22,9 @@ public class CommunicationScript : MonoBehaviour
         timeSinceLastMessage = 0;
         if (textDisplay != null)
             textDisplay.text = "Connecting..."; //If you have a TextMesh, this will set its text.
+        foot.GetComponent<Rigidbody>().detectCollisions = false;
+        foot.transform.position = startPosition;
+        move = false;
     }
 
     // Update is called once per frame
@@ -28,29 +34,44 @@ public class CommunicationScript : MonoBehaviour
     //     // connector.SendMessage("This is a test message"); //This is how you send messages to the ESP.
     // }
 
+    public void ChangeMove() {
+        move = !move;
+    }
+
     public void onMessageRecieved(string message) //Things inside this function will happen when the quest recieves a message from the ESP.
     {
         timeSinceLastMessage = 0;
+        if (message.Trim().Equals("reset")) {
+            foot.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            foot.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            foot.transform.rotation = Quaternion.identity;
+            foot.transform.position = startPosition;
+            return;
+        }
         if (textDisplay != null) {
             string[] str_split = message.Split(',');
-            bool moving = false;
 
-            List<float> vals = new List<float>(6);   // in order acceleration x, y, z, gyro x, y, z
+            List<float> vals = new List<float>(10);   // in order foot, acceleration x, y, z, gyro x, y, z, mag x, y, z
             int i = 0;
             foreach (var sub in str_split) {
                 float cur = float.Parse(sub, CultureInfo.InvariantCulture.NumberFormat);
                 vals.Add(cur);
-                if (i < 3 && cur > acc_threshold) {
-                    moving = true;
-                }
+                i++;
             }
+            string toDisplay = "foot: " + vals[0] + "\n" +
+                               "xAcc: " + vals[1] + "\n" +
+                               "yAcc: " + vals[2] + "\n" +
+                               "zAcc: " + vals[3] + "\n" +
+                               "xRot: " + vals[4] + "\n" +
+                               "yRot: " + vals[5] + "\n" +
+                               "zRot: " + vals[6];
 
-            if (moving) {
-                textDisplay.text = "ESP32 is currently moving";
-            } else {
-                textDisplay.text = "ESP32 is currently still";
+            textDisplay.text = toDisplay;
+            if (move) {
+                foot.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(-vals[2], -vals[3], vals[1])*foot.GetComponent<Rigidbody>().mass);
+                foot.GetComponent<Rigidbody>().AddForce(new Vector3(0, 9.8f, 0)*foot.GetComponent<Rigidbody>().mass);
             }
-            // textDisplay.text = message;
+            foot.GetComponent<Rigidbody>().angularVelocity = foot.transform.TransformDirection(new Vector3(-vals[5], -vals[6], vals[4]));
         }
     }
 
@@ -64,6 +85,9 @@ public class CommunicationScript : MonoBehaviour
         timeSinceLastMessage += Time.deltaTime;
         if (timeDisplay != null) {
             timeDisplay.text = timeSinceLastMessage.ToString();
+        }
+        if (move) {
+            foot.GetComponent<Rigidbody>().AddForce(-foot.GetComponent<Rigidbody>().velocity*0.01f*Time.deltaTime*foot.GetComponent<Rigidbody>().mass);
         }
     }
 }
